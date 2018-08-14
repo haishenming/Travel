589371690e69119b840696a3c8ba31d1fbdbc270
haishenming <haishenmingx@gmail.com>
2018-08-14 21:20:19 +0800

    init

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -1,7 +1,6 @@
 <template>
   <div>
-    <div class="home">Home</div>
-    <router-link to="/list" class="home">列表页</router-link>
+    Hello word
   </div>
 </template>
 
@@ -12,7 +11,4 @@ export default {
 </script>
 
 <style scoped>
-  .home {
-    font-size: 50px;
-  }
 </style>
