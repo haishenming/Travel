b67c6a643bbd64fc90e96cecb8037c51dbf418fc
haishenming <haishenmingx@gmail.com>
2018-08-19 20:53:59 +0800

    fix(src): header

--- src/pages/home/components/Header.vue
+++ src/pages/home/components/Header.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="header">
-    <div class="header-left">返回</div>
-    <div class="header-input">内容</div>
-    <div class="header-right">城市</div>
+    <div class="header-left">
+      <div class="iconfont icon-fanhui back-icon"></div>
+    </div>
+    <div class="header-input">
+      <span class="iconfont icon-sousuo"></span>
+      输入城市/景点/游玩/主题</div>
+    <div class="header-right">城市
+      <span class="iconfont icon-jiantou arrow-icon"></span>
+    </div>
   </div>
 </template>
 
@@ -12,16 +18,35 @@ export default {
 }
 </script>
 
-<style scoped lang="stylus">
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
   .header
-    height: .86rem
+    display: flex
+    line-height: .86rem
+    background: $bgColor
+    color: #ffffff
     .header-left
       width: .64rem
       float: left
+      .back-icon
+        text-align: center
+        font-size: .4rem
     .header-input
       flex: 1
+      height: .64rem
+      line-height:.64rem
+      margin-top: .12rem
+      margin-left: .2rem
+      padding-left: .2rem
+      background #ffffff
+      border-radius: .1rem
+      color: #ccc
     .header-right
       width: 1.24rem
       float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
 
 </style>
