58b95519e3bd73498e6a980fabff15f2ee80d39b
haishenming <haishenmingx@gmail.com>
2018-08-19 21:56:46 +0800

    fix(Home): Swiper

---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item in swiperList" :key="item.id">
+        <img class="swiper-img"
+             :src="item.imgUrl">
+      </swiper-slide>
+      <div class="swiper-pagination" slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data: function () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination', // 添加轮播图下方的小点
+        loop: true // 支持循环轮播
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/dujia_shuqi_banner_20180710.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'https://source.qunarzz.com/site/images/wap/home/recommend/iphoneplus/sc_qixi_20180817.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullet-active
+    background: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 24%
+    background: #eee
+    .swiper-img
+      width: 100%
+</style>
